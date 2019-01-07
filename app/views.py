9497a53d56f0318255303ba9c700bc73854a9841
kris <zhiyong.wu@bjtce.com>
2019-01-07 17:28:04 +0800

    add invoking interface duration

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import json
 from flask import jsonify
 import base64
 import time
+import datetime
 
 
 @app.route('/')
@@ -16,6 +17,7 @@ def index():
     return render_template('index.html', title='首页')
 
 
+# 人证比对
 @app.route('/match', methods=['GET', 'POST'])
 def match():
     if request.method == 'GET':
@@ -24,12 +26,14 @@ def match():
         return render_template('match.html', title='POST')
 
 
+# 活体检测
 @app.route('/liveness', methods=['GET', 'POST'])
 def liveness():
     if request.method == 'GET':
         return render_template('liveness.html', title='活体检测')
 
 
+# base64转图片
 @app.route('/b64toimg', methods=['GET', 'POST'])
 def b64toimg():
     if request.method == 'GET':
@@ -47,6 +51,7 @@ def b64toimg():
             return jsonify({'msg': 'transfer failed.', "img_path": "static/resources/null.jpg"})
 
 
+# 图片转base64
 @app.route('/imgtob64', methods=['GET', 'POST'])
 def imgtob64():
     if request.method == 'GET':
@@ -63,7 +68,7 @@ def imgtob64():
         return jsonify({'msg': "transfer successfully.", "base64": b64_code})
 
 
-# 接口
+# ajax接口
 @app.route('/api/match', methods=['POST'])
 def api_match():
     if request.method == 'POST':
@@ -74,7 +79,10 @@ def api_match():
         idcard.save(idcard_path)
         live.save(live_path)
         # 调用接口，第一个参数是身份证地址，第二个参数是现场图片
+        start_time = datetime.datetime.now()
         result = mth(idcard_path, live_path)
+        end_time = datetime.datetime.now()
+        duration = end_time - start_time
         d_result = json.loads(result)
 
         # 判断是否比对成功
@@ -82,7 +90,7 @@ def api_match():
 
         if error_code == 0:
             score = d_result.get('result').get('score')
-            return jsonify({'msg': score})
+            return jsonify({'msg': score, 'duration': (duration.microseconds / 1000)})
         else:
             error_msg = d_result.get('error_msg')
             print(error_msg)
@@ -95,14 +103,17 @@ def api_liveness():
         img_file = request.files['file_img']
         img_file_path = os.path.join(os.getcwd(), r'app/static/resources', img_file.filename)
         img_file.save(img_file_path)
+        start_time = datetime.datetime.now()
         result = lv(img_file_path)
+        end_time = datetime.datetime.now()
+        duration = end_time - start_time
         d_result = json.loads(result)
         # 判断是否认证成功
         error_code = d_result.get('error_code')
 
         if error_code == 0:
             score = d_result.get('result').get('face_liveness')
-            return jsonify({'msg': score})
+            return jsonify({'msg': score, 'duration': (duration.microseconds / 1000)})
         else:
             error_msg = d_result.get('error_msg')
             print(error_msg)
